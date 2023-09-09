d51ea98e1c6c0358ee7533d2e3c7789e8c29b6d3
Tanmay Maitra Tanu <77947828+tanmaytanu@users.noreply.github.com>
2023-09-09 14:00:35 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// text change
+function text_change() {
+    document.getElementById('h2').innerHTML="My name is Tanmay Maitra Tanu";
+}
+
+function t_change() {
+    document.getElementById('h3').innerHTML="My name is Tanmay Maitra Tanu";
+}
+
+function te_change() {
+    document.getElementById('h4').innerHTML="My name is Tanmay Maitra Tanu";
+}
+
+function tex_change() {
+    document.getElementById('h5').innerHTML="My name is Tanmay Maitra Tanu";
+}
+
+function textchange() {
+    document.getElementById('h6').innerHTML="My name is Tanmay Maitra Tanu";
+}
+
+function text_hange() {
+    document.getElementById('h7').innerHTML="My name is Tanmay Maitra Tanu";
+}
+
+function text_cange() {
+    document.getElementById('h8').innerHTML="My name is Tanmay Maitra Tanu";
+}
+
+function text_chnge() {
+    document.getElementById('h9').innerHTML="My name is Tanmay Maitra Tanu";
+}
+
+function text_chage() {
+    document.getElementById('h10').innerHTML="My name is Tanmay Maitra Tanu";
+}
+
+function text_chane() {
+    document.getElementById('h11').innerHTML="My name is Tanmay Maitra Tanu";
+}
